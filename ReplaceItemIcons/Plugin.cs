adad10e7b67a6cf60a5d53c5dde739a3164e59a5
PresentKim <debe3721@gmail.com>
2026-06-13 20:10:13 +0900

    feat: Init project

---
+++ ReplaceItemIcons/Plugin.cs
@@ -0,0 +1,9 @@
+using BepInEx;
+
+namespace kim.present.lumaisland.replaceitemicons
+{
+    [BepInPlugin("kim.present.lumaisland.replaceitemicons", "ReplaceItemIcons", "0.0.1")]
+    public class ReplaceItemIcons : BaseUnityPlugin
+    {
+    }
+}
